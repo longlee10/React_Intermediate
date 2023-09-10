c244b1dff434e24dd5cc939f53a11c52d0d0bff3
longlee10 <longquan98@gmail.com>
2023-09-10 16:55:45 -0400

    handling success and error in optimistic approach

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -3,23 +3,49 @@ import axios from "axios";
 import { useRef } from "react";
 import { Todo } from "./hooks/useTodos";
 
+interface TodoContext {
+  prevTodos: Todo[];
+}
+
 const TodoForm = () => {
   const queryClient = useQueryClient();
   const ref = useRef<HTMLInputElement>(null);
 
-  const addTodo = useMutation<Todo, Error, Todo>({
+  const addTodo = useMutation<Todo, Error, Todo, TodoContext>({
     mutationFn: (todo: Todo) =>
       axios
-        .post<Todo>("https://jsonplaceholder.typicode.com/posts", todo)
+        .post<Todo>("https://xjsonplaceholder.typicode.com/posts", todo)
         .then((res) => res.data),
-    // if post request success: update the todo list
-    onSuccess: (savedTodo, newTodo) => {
+
+    // Optimistic approach
+    onMutate: (newTodo: Todo) => {
+      const prevTodos = queryClient.getQueryData<Todo[]>(["todos"]) || [];
       // Approach 1 invalidating cache -> Use the queryClient object. Not working in JSON place holder
       // Approach 2: Updating data in cache
       queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
-        savedTodo,
+        newTodo,
         ...(todos || []),
       ]);
+
+      if (ref.current) ref.current.value = "";
+
+      return { prevTodos };
+    },
+
+    // if post request success: update the todo list (Pessimistic approach)
+    // Now with optimistic approach, in onSuccess success scenarios
+    onSuccess: (savedTodo, newTodo) => {
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) =>
+        todos?.map((todo) => (todo === newTodo ? savedTodo : todo))
+      );
+    },
+
+    // Handling error scenario
+    onError: (err, newTodo, context) => {
+      if (!context) return;
+
+      // returning the previous UI
+      queryClient.setQueryData<Todo[]>(["todos"], context.prevTodos);
     },
   });
 
@@ -47,7 +73,9 @@ const TodoForm = () => {
           <input ref={ref} type="text" className="form-control" />
         </div>
         <div className="col">
-          <button className="btn btn-primary">Add</button>
+          <button className="btn btn-primary" disabled={addTodo.isLoading}>
+            {addTodo.isLoading ? "Adding..." : "Add"}
+          </button>
         </div>
       </form>
     </>
