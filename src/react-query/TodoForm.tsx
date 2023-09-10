be9cf5e467f69ddb4650bf8b48ef338667ba817c
longlee10 <longquan98@gmail.com>
2023-09-10 16:27:52 -0400

    mutating data: handling error

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -1,17 +1,56 @@
-import { useRef } from 'react';
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import { useRef } from "react";
+import { Todo } from "./hooks/useTodos";
 
 const TodoForm = () => {
+  const queryClient = useQueryClient();
   const ref = useRef<HTMLInputElement>(null);
 
+  const addTodo = useMutation<Todo, Error, Todo>({
+    mutationFn: (todo: Todo) =>
+      axios
+        .post<Todo>("https://jsonplaceholder.typicode.com/posts", todo)
+        .then((res) => res.data),
+    // if post request success: update the todo list
+    onSuccess: (savedTodo, newTodo) => {
+      // Approach 1 invalidating cache -> Use the queryClient object. Not working in JSON place holder
+      // Approach 2: Updating data in cache
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
+        savedTodo,
+        ...(todos || []),
+      ]);
+    },
+  });
+
   return (
-    <form className="row mb-3">
-      <div className="col">
-        <input ref={ref} type="text" className="form-control" />
-      </div>
-      <div className="col">
-        <button className="btn btn-primary">Add</button>
-      </div>
-    </form>
+    <>
+      {addTodo.error && (
+        <div className="alert alert-danger">{addTodo.error.message}</div>
+      )}
+      <form
+        className="row mb-3"
+        onSubmit={(e) => {
+          e.preventDefault();
+          // mutating data
+          if (ref.current && ref.current.value) {
+            addTodo.mutate({
+              id: 1000,
+              title: ref.current?.value,
+              completed: false,
+              userId: 1,
+            });
+          }
+        }}
+      >
+        <div className="col">
+          <input ref={ref} type="text" className="form-control" />
+        </div>
+        <div className="col">
+          <button className="btn btn-primary">Add</button>
+        </div>
+      </form>
+    </>
   );
 };
 
