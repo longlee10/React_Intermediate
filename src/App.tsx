372f52cf5c163d96d8b9de9533b1abd0316310af
longlee10 <longquan98@gmail.com>
2023-09-12 20:54:17 -0400

    create taskReducer

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import PostList from "./react-query/PostList";
 import TodoForm from "./react-query/TodoForm";
 import TodoList from "./react-query/TodoList";
 import Counter from "./state-management/Counter";
+import TaskList from "./state-management/TaskList";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       {/* <TodoForm />
       <TodoList /> */}
       <Counter />
+      <TaskList />
     </>
   );
 }
---
+++ src/state-management/reducer/taskReducer.ts
@@ -0,0 +1,26 @@
+interface Task {
+  id: number;
+  title: string;
+}
+
+interface AddTask {
+  type: "ADD";
+  task: Task;
+}
+
+interface DeleteTask {
+  type: "DELETE";
+  taskId: number;
+}
+type Action = AddTask | DeleteTask;
+
+const taskReducer = (tasks: Task[], action: Action): Task[] => {
+  switch (action.type) {
+    case "ADD":
+      return [action.task, ...tasks];
+    case "DELETE":
+      return tasks.filter((t) => t.id !== action.taskId);
+  }
+};
+
+export default taskReducer;
