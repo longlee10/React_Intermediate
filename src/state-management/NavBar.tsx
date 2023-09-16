654f190f50e0a35fdc104db7c0aeb94cf0019a36
longlee10 <longquan98@gmail.com>
2023-09-16 01:11:15 -0400

    managing state of counter with zudstand

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -2,13 +2,15 @@ import { useContext } from "react";
 import LoginStatus from "./auth/LoginStatus";
 import TaskContext from "./tasks/taskContext";
 import UserContext from "./auth/userContext";
+import useCounterStore from "./counter/store";
 
 const NavBar = () => {
   const { tasks } = useContext(TaskContext);
+  const { counter } = useCounterStore();
 
   return (
     <nav className="navbar d-flex justify-content-between">
-      <span className="badge text-bg-secondary">{tasks.length}</span>
+      <span className="badge text-bg-secondary">{counter}</span>
       <LoginStatus />
     </nav>
   );
