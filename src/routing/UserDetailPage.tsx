0526bbf9eef48db1aa7b2e7bbb79643820b11e31
unknown <longquan98@gmail.com>
2023-09-23 11:51:27 -0400

    nested router

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,4 +1,12 @@
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
+  const params = useParams();
+  console.log(params);
+  const [searchParams, setSearchParams] = useSearchParams();
+  console.log(searchParams.get("name")); //add this to url: ?name=Alice -> this is for searching purpose
+  const location = useLocation();
+  console.log(location); // current location in the url
   return <p>User</p>;
 };
 
--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -2,11 +2,20 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./HomePage";
 import UserListPage from "./UserListPage";
 import ContactPage from "./ContactPage";
+import UserDetailPage from "./UserDetailPage";
+import Layout from "./Layout";
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/users", element: <UserListPage /> },
-  { path: "/contact", element: <ContactPage /> },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <HomePage /> }, // index : true means the default element to be rendered
+      { path: "users", element: <UserListPage /> },
+      { path: "users/:id", element: <UserDetailPage /> },
+      { path: "contact", element: <ContactPage /> },
+    ],
+  },
 ]);
 
 export default router;
